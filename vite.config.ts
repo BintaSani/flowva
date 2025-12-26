57cae5791410ca1e8d397481a4b5086a455ef706
BintaSani <bintasani992@gmail.com>
2025-12-26 04:10:04 +0100

    middleware

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import history from "connect-history-api-fallback";
+import type { Connect } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "spa-fallback",
+      configureServer(server) {
+        // explicitly cast to the type Connect.NextHandleFunction
+        const middleware = history({
+          index: "/index.html",
+          htmlAcceptHeaders: ["text/html", "application/xhtml+xml"],
+        }) as unknown as Connect.NextHandleFunction;
+
+        server.middlewares.use(middleware);
+      },
+    },
+  ],
 });
