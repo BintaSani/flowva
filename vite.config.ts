11108749c39c859f7986d4e412133a26a8cef770
BintaSani <bintasani992@gmail.com>
2025-12-26 04:26:41 +0100

    fixed type issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import history from "connect-history-api-fallback";
-import type { Connect } from "vite";
+// import history from "connect-history-api-fallback";
+// import type { Connect } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
